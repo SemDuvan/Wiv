0e99b93b4cc22f415568bb8b46bd384d69c3dcc5
SemDuvan <2403737duvan@zuyd.nl>
2025-01-17 09:16:30 +0100

    Push

---
+++ map/Index.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace map
+{
+    public partial class Index : Window
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void Waarneming_Click(object sender, RoutedEventArgs e)
+        {
+            WaarnemingWindow waarnemingWindow = new WaarnemingWindow();
+            waarnemingWindow.Show();
+            this.Close();
+        }
+
+        private void Profile_Click(object sender, RoutedEventArgs e)
+        {
+            ProfileWindow profileWindow = new ProfileWindow();
+            profileWindow.Show();
+            this.Close();
+        }
+
+        private void Navigatie_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
