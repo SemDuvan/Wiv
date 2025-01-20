c23b4e074b81a2a0750e149c1557da140aae3a63
SemDuvan <2403737duvan@zuyd.nl>
2025-01-20 08:53:22 +0100

    Locatie toegevoegt bij waarneming +Back Knop

---
+++ Back-end/Map API/map/GetLocation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Device.Location;
+using System.Threading.Tasks;
+
+namespace map
+{
+    public class LocationService
+    {
+        public async Task<(double Latitude, double Longitude)> GetLocationAsync()
+        {
+            var tcs = new TaskCompletionSource<GeoCoordinate>();
+            var watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+            watcher.MovementThreshold = 1.0; // Gebruik een drempel van 1 meter voor updates
+
+            watcher.StatusChanged += (s, e) =>
+            {
+                if (e.Status == GeoPositionStatus.Ready && !watcher.Position.Location.IsUnknown)
+                {
+                    tcs.TrySetResult(watcher.Position.Location);
+                    watcher.Stop();
+                }
+            };
+
+            watcher.Start();
+
+            var coord = await tcs.Task.ConfigureAwait(false);
+            return (coord.Latitude, coord.Longitude);
+        }
+    }
+}
+
--- Back-end/Map API/map/WaarnemingWindow.xaml.cs
+++ Back-end/Map API/map/WaarnemingWindow.xaml.cs
@@ -18,10 +18,12 @@ namespace map
     public partial class WaarnemingWindow : Window
     {
         private string selectedCategory;
+        private LocationService locationService;
 
         public WaarnemingWindow()
         {
             InitializeComponent();
+            locationService = new LocationService();
         }
 
         private void UploadImage_Click(object sender, RoutedEventArgs e)
@@ -44,12 +46,26 @@ namespace map
             selectedCategory = "Fauna";
         }
 
-        private void Verzend_Click(object sender, RoutedEventArgs e)
+        private async void Verzend_Click(object sender, RoutedEventArgs e)
         {
             string name = nameTextBox.Text;
             string description = descriptionTextBox.Text;
             string dateTime = DateTime.Now.ToString("g"); // "g" format geeft een korte datum en tijd weer
-            MessageBox.Show($"{selectedCategory} {name} {description}\nDatum en tijd: {dateTime}", "Checkcheck");
+            var location = await locationService.GetLocationAsync();
+            if (location.Latitude == 0 && location.Longitude == 0)
+            {
+                MessageBox.Show($"{selectedCategory} {name} {description}\nDatum en tijd: {dateTime}\nLocatie onbekend", "Invoer");
+            }
+            else
+            {
+                MessageBox.Show($"{selectedCategory} {name} {description}\nDatum en tijd: {dateTime}\nLatitude: {location.Latitude}, Longitude: {location.Longitude}", "Invoer");
+            }
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Index Index = new Index();
+            Index.Show();
+            this.Close();
         }
     }
 }
